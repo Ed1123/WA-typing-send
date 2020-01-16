ccd747d11a5cae323e67fca793e8890180950e92
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2020-01-15 22:46:18 -0500

    A sendMessage function
    Thanks to Renan Coelho on stackoverflow for the code that I adapted to mine. Thanks to that, the code now sends personalized messages every second (before it just click on the send button every second).
    - Added the sendMessage function for more readability.
    - Using querySelector instead of ClassName. With ClassName wasn't posible to custom the message from the console.
    - Added a command to stop the sending of messages.
    - Some minor typing errors.
    
    https://stackoverflow.com/questions/47243154/how-to-send-whatsapp-message-via-javascript

--- send.js
+++ send.js
@@ -7,13 +7,39 @@ something like: "Let me properly call [random_name]. Hey, [insert a beautiful ni
 be sent just after he starts typing his response and sends it proving you know how to properly call him.
 */
 
+
+// Function that sends a message. It just type and click on send.
+function sendMessage (message) {
+	// Textbox query
+	textBoxQuery = '#main > footer > div._2i7Ej._14Mgc.copyable-area > div._13mgZ > div > div._3u328.copyable-text.selectable-text';
+
+	// Send button query name by the time of codding
+	buttonQuery = '#main > footer > div._2i7Ej._14Mgc.copyable-area > div:nth-child(3) > button';
+
+	// textBox Object for making the code more readable
+	textBox = document.querySelector(textBoxQuery);
+
+	// Typing the text
+	textBox.textContent = message;
+
+	
+	var event = new InputEvent('input', {
+		bubbles: true
+	}); // I don't know yet what is this for, but is needed for activating the textbox.
+	
+	// Activating the textBox to make the send button visible
+	textBox.dispatchEvent(event);
+
+	// Pressing the send button
+	document.querySelector(buttonQuery).click();
+}
+
+message = "You're typing";
+
 contactName = 'Mishell';
 isTypingText = contactName +  ' is typing…';
 
 subTitleTextClassName = '_315-i _F7Vk'; // behind the contact name. The area where 'typing...' is displayed.
-
-// Send button class name by the time of codding: '_3M-N-'
-buttonClassName = '_3M-N-';
 
 /*while the text is not 'contactName is typing…' just keep in the loop.
 When the contact starts typing (the text is iqual) the loop ends and the code continues.*/
@@ -23,12 +49,15 @@ while (isTypingText != document.getElementsByClassName(subTitleTextClassName)[0]
 };
 */
 // The code above was change for a better implementation using setInterval
-setInterval(function(){
+interval = setInterval(function(){
 		if (isTypingText == document.getElementsByClassName(subTitleTextClassName)[0].title) {
-			// Pressing the send button
-			document.getElementsByClassName('buttonClassName')[0].click();	
-			console.log('Message sent.')
+			
+			sendMessage(message);
+			console.log('Message sent.');
 		}
-        console.log('Not yet') // Looged each second
+        console.log('Not yet'); // Looged each second
 
      },1000);
+
+// To stop the code update the page or use the following command:
+clearInterval(interval);
